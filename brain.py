8bc435a20cb36fe0eb8fa032477e40b188dd07c0
Ioan-Alexandru <53114650+alexonati@users.noreply.github.com>
2021-09-28 11:57:19 +0300

    ML small things
    Used some basic techniques to understand better ML practices.

---
+++ brain.py
@@ -0,0 +1,16 @@
+'''pip install -U tensorflow keras opencv-python'''
+
+from imageai.Prediction import ImagePrediction
+import os
+execution_path=os.getcwd()
+
+''' Images have to be in the same directory for it to work.'''
+
+prediction = ImagePrediction()
+prediction.setModelTypeAsSqueezeNet()
+prediction.setModelPath(os.path.join(execution_path, "squeezenet_weights_tf_dim_ordering_tf_kernels.h5"))
+prediction.loadModel()
+
+predictions, probabilities = prediction.predictImage(os.path.join(execution_path, "IMAGE.jpg"), result_count=5 )
+for eachPrediction, eachProbability in zip(predictions, probabilities):
+    print(eachPrediction , " : " , eachProbability)
